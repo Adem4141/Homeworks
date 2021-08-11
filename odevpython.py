9720f525f7c0e9a5bb0e63ae34b6da214e53a214
Adem Saraç <83869675+Adem4141@users.noreply.github.com>
2021-08-12 01:52:08 +0300

    Create odevpython.py

---
+++ odevpython.py
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def fun(liste):
+    new_list = list(reversed(liste))
+    for i in new_list:
+        i.reverse()
+        
+    return new_list
+
+print(fun(liste))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
