b3ebfcb2ac64d8fa482d49a21eae6320eedf76aa
Adem Saraç <83869675+Adem4141@users.noreply.github.com>
2021-08-24 19:21:43 +0300

    Update odevpython.py

--- odevpython.py
+++ odevpython.py
@@ -1,4 +1,25 @@
 
+from collections.abc import Iterable
+ 
+former_list = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+
+
+new_list = []
+def flatten_list(list):
+        
+    for i in list:
+        if isinstance(i, Iterable) and type(i) != str:
+            flatten_list(i)
+                
+        else:
+            new_list.append(i)
+            
+    
+    return new_list
+    
+
+flatten_list(liste)
+
 
 
 
